2f451fb2442a25fa49a1d2a57a6b26fac26cdad4
MatthewC00273575 <ufumeli2@gmail.com>
2024-02-26 22:43:44 +0000

    different pages added

--- lib/components/auth_page.dart
+++ lib/components/auth_page.dart
@@ -14,8 +14,9 @@ class AuthPage extends StatelessWidget {
         builder: (context, snapshot) {
           // user is logged in
           if (snapshot.hasData) {
-            return const HomePage();
+            return HomePage();
           }
+
           //user is not logged in
           else {
             return LoginPage();
